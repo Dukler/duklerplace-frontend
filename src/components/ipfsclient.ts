dbfc21ca8d3ebf314cd87ee8e3f7c7dabc8e8dd9
Dukler <iarwain@hotmail.com.ar>
2022-10-21 20:18:36 -0300

    ipfs client repo lock fixed

--- src/components/ipfsclient.ts
+++ src/components/ipfsclient.ts
@@ -3,7 +3,7 @@ type IPFSType = Awaited<ReturnType<typeof IPFSCORE.create>>
 let ipfsclient:IPFSType = {} as IPFSType
 let ready = false
 if(!ready) {
-  ipfsclient = await IPFSCORE.create()
+  ipfsclient = await IPFSCORE.create({ repo: "ok" + Math.random() })
   ready = true
 }
 
